297309aa103202df47d06666c4ddf658048a2042
Ahmad Nikmanesh <anikmanesh@mail.csuchico.edu>
2019-02-13 11:50:21 -0800

    updates My Groceries

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+	vector<string> list;
+	int numItems = 0;
+	char input;
+	string item; 
+	while(input != 'Q')
+	{
+		cout<<"\n==GROCERY LIST MANAGER==";
+		cout<<"\nEnter your choice: ";
+		cout<<"\n (A)dd an item";
+		cout<<"\n (Q)uit";
+		cout<<"\nYour choice (A/Q): ";
+		cin>>input;
+		if(input == 'A')
+		{
+			cout <<"\nname of item: ";
+			cin >> item;
+			list.push_back(item);  
+		numItems++;
+		}
+	}
+	if (numItems == 0)
+	{
+		cout << "There are no items on the list" <<endl;
+	}
+	else
+	{
+		for(int i=0;i<numItems;i++)
+		{
+			cout << i+1 <<": "<<  list[i] << endl;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-
-  return 0;
+		}
+	}
+	return 0;
 }
